8f79aa1f07f85b560581ab4780e9ef76768d727c
Federico Bricker <fbricker@gmail.com>
2014-03-19 08:45:11 -0300

    Shape the project

---
+++ dependencies/gpgex/src/com/gpgex/GooglePlayGames.java
@@ -0,0 +1,100 @@
+package com.gcmex;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.app.Activity;
+import org.haxe.extension.Extension;
+
+import com.google.android.gms.games.Games;
+import com.google.android.gms.games.Player;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.ConnectionResult;
+
+public class PlayGames extends Extension implements GameHelper.GameHelperListener {
+	
+	private static PlayGames instance=null;
+	private static GameHelper mHelper=null;
+	public static final String TAG = "OPENFL-GPG";
+
+	public static PlayGames getInstance(){
+		if(instance==null) instance=new PlayGames();
+		return instance;
+	}
+
+	public static void init(){
+		if(mHelper!=null){
+			if(!mHelper.isConnecting()) return;
+			mHelper=null;
+		}
+
+		mainActivity.runOnUiThread(new Runnable() {
+            public void run() { 
+		        Log.i(TAG, "PlayGames: INIT CALL");
+				mHelper = new GameHelper(mainActivity, GameHelper.CLIENT_GAMES);
+				mHelper.enableDebugLog(true);
+				mHelper.setup(GooglePlayGames.getInstance());
+				mHelper.setMaxAutoSignInAttempts(3);
+				mHelper.onStart(mainActivity);
+				Log.i(TAG, "PlayGames: INIT COMPLETE");
+            }
+        });
+	}	
+
+	public static void connect(){
+        Log.i(TAG, "PlayGames: CONNECT begin");
+		if(mHelper.isSignedIn()){
+	        Log.i(TAG, "PlayGames: - CONNECT - Doing nothing... Already SignedIn");
+			return;
+		}
+		if(mHelper.isConnecting()){
+	        Log.i(TAG, "PlayGames: - CONNECT - Doing nothing... Still connecting");
+			return;
+		}
+	    mHelper.beginUserInitiatedSignIn();
+        Log.i(TAG, "PlayGames: CONNECT complete");
+     }
+
+	public static void setScore(String id, Int score){
+		if(mHelper==null){
+	        Log.i(TAG, "PlayGames: useLeaderBoard - YOU MUST CALL INIT FIRST!");
+			return;
+		}
+		if(mHelper.isConnecting()){
+	        Log.i(TAG, "PlayGames: useLeaderBoard - WAIT... Still connecting!");
+			return;
+		}
+		if(!mHelper.isSignedIn()){
+	        Log.i(TAG, "PlayGames: useLeaderBoard - Not signed in!");
+	        connect();
+			return;
+		}
+        Log.i(TAG, "PlayGames: useLeaderBoard begin!");
+		Games.Leaderboards.submitScore(mHelper.mGoogleApiClient, id, score);
+        Log.i(TAG, "PlayGames: useLeaderBoard complete");
+	}
+
+	public static void displayScoreBoard(String id){
+		try {
+			mainActivity.startActivityForResult(Games.Leaderboards.getLeaderboardIntent(mHelper.mGoogleApiClient, id), 0);
+		} catch (Exception e) {
+			// Try connecting again
+			Log.i(TAG, "PlayGames: displayScoreBoard Exception");
+			Log.i(TAG, e.toString());
+			mHelper=null;
+			init();
+		}
+	}
+
+	@Override
+    public void onSignInFailed() {
+        Log.i(TAG, "PlayGames: onSignInFailed");
+    }
+
+    @Override
+    public void onSignInSucceeded() {
+        Log.i(TAG, "PlayGames: onSignInSucceeded");
+    }
+
+}
--- gpgex/GooglePlayGames.hx
+++ gpgex/GooglePlayGames.hx
@@ -1,53 +1,19 @@
 package gpgex;
 
 class GooglePlayGames {
-	public static var checkPlayServices(default,null):Void->Bool=
+
+	public static var init(default,null):Void->Void=
 	#if android
-		openfl.utils.JNI.createStaticMethod("com/gcmex/GCM", "checkPlayServices", "()Z");
+		openfl.utils.JNI.createStaticMethod("com/gpgex/GooglePlayGames", "init", "()V");
 	#else
-		function():Bool{return false;}
+		function():Void{}
 	#end
 
-	public static var getRegistrationId(default,null):Void->String=
+	public static var displayScoreBoard(default,null):String->Void=
 	#if android
-		openfl.utils.JNI.createStaticMethod("com/gcmex/GCM", "getRegistrationId", "()Ljava/lang/String;");
-	#else
-		function():String{return null;}
-	#end
-
-	private static var realInit(default,null):String->Dynamic->Void=
-	#if android
-		openfl.utils.JNI.createStaticMethod("com/gcmex/GCM", "init", "(Ljava/lang/String;Lorg/haxe/lime/HaxeObject;)V");
-	#else
-		function(senderId:String,callback:Dynamic):Void{}
-	#end
-
-	public static var sendMessage(default,null):String->Void=
-	#if android
-		openfl.utils.JNI.createStaticMethod("com/gcmex/GCM", "sendMessage", "(Ljava/lang/String;)V");
+		openfl.utils.JNI.createStaticMethod("com/gpgex/GooglePlayGames", "displayScoreBoard", "(Ljava/lang/String;)V");
 	#else
 		function(msg:String):Void{}
 	#end
 
-	public static function init(senderId:String){
-		realInit(senderId,getInstance());
-	}
-
-	private static function getInstance():GCM{
-		if(instance==null) instance=new GCM();
-		return instance;
-	}
-
-	private static var instance:GCM=null;
-	public static var onReceive:String->String->Void=null;
-	
-	private function new(){}
-
-	public function receiveCallback(type:String, json:String){
-		if(onReceive==null){
-			trace("RECEIVED: "+type+" = "+json);
-		}else{
-			onReceive(type,json);
-		}
-	}
 }
