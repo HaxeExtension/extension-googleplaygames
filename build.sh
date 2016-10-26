75e4e09f407c4f68cf961d1d33fe40f617ab4591
Federico Bricker <fbricker@gmail.com>
2016-10-26 19:42:39 -0300

    Minimize dependencies

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 dir=`dirname "$0"`
 cd "$dir"
 rm -f extension-googleplaygames.zip
-zip -0r extension-googleplaygames.zip extension haxelib.json include.xml dependencies 
+zip -9r extension-googleplaygames.zip extension haxelib.json include.xml dependencies 
